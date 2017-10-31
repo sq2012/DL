904e05b5d09b02c342e7278bff3a00ec669cbcac
sq2012 <31462327+sq2012@users.noreply.github.com>
2017-10-31 21:33:32 +0800

    Update sentiment_words.py

--- ccf/sentiment_words.py
+++ ccf/sentiment_words.py
@@ -1,10 +1,4 @@
-# https://github.com/EliasCai/sentiment 代码持续更新，欢迎大家关注，希望有所帮助，共同提升 
-# 个人介绍：工作从事大数据开发，熟悉机器学习和深度学习的使用
-# 比赛经验：曾参加场景分类（AiChallenger）、口碑商家客流量预测（天池）、用户贷款风险预测（DataCastle）、
-#           摩拜算法天战赛（biendata）等，寻找队友冲击前排，希望不吝收留！
-# 版本：v1.1
 # 环境：python3; tensorflow-1.0.0; keras-2.0.6
-# 邮箱：elias8888#qq.com
 # 使用：将data文件夹中的三个csv文件放到py文件同个文件夹下面即可运行
 # Finish：
 # 使用jieba进行分词，并用LSTM对第一个情感关键词进行预测，10轮epochs后验证样本的准确率为0.70
